dc9d2e098cdba6a3411dfa5d15e87566ee4572c8
abdel198 <abdellazizaarab@hotmail.com>
2017-02-27 21:24:41 +0100

    serial_2_5

--- serial_2_5/serial_2_5.ino
+++ serial_2_5/serial_2_5.ino
@@ -19,9 +19,7 @@ int drive_mb;
   Serial.print("Your HD is ");
   Serial.print(drive_gb);
   Serial.println(" GB large.");
-
-  drive_mb = 1024 * drive_gb;
-
+  drive_mb = -1024 * drive_gb;
   Serial.print("It can store ");
   Serial.print(drive_mb);
   Serial.println(" Megabytes!");
