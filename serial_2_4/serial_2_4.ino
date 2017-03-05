64bf4817c99ac216879e97194bf7402d926555e0
abdel198 <abdellazizaarab@hotmail.com>
2017-03-05 18:25:50 +0100

    subida

--- serial_2_4/serial_2_4.ino
+++ serial_2_4/serial_2_4.ino
@@ -9,27 +9,27 @@
 
 //******************************* VARIABLES ***************************************
 //Definir la variable
-int drive_gb = 100; //define 100 GB
+int drive_gb = 100; //Define 100 GB
 int drive_mb; // Esta variable cambia el formato
 //******************************** SETUP ******************************************
 void setup()             
 {
-Serial.begin(9600);      // jecutar una cuando se inicia el arduino
+Serial.begin(9600);      // Ejecutar una vez cuando se inicia el arduino
   Serial.print("Your HD is "); // Primer texto en primera línea en el terminal
   Serial.print(drive_gb); // Puntua el número definido antes
   Serial.println(" GB large."); // Terminar la línea
 
-  drive_mb = 1024 * drive_gb; //define the conversor type between GB and Megabytes
+  drive_mb = 1024 * drive_gb; //Definir el tipo de conversor entre GB y Megabytes
 
-  Serial.print("It can store "); //first text on second line in terminal
-  Serial.print(drive_mb); // the number of multiply 1024*100
-  Serial.println(" Megabytes!"); // to end the line
-  // The result is : Your HD is 100 GB large.It can store -28672 Megabytes!
+  Serial.print("It can store "); //Primer texto en la segunda línea en el terminal
+  Serial.print(drive_mb); // El número de multiplicar 1024 * 100
+  Serial.println(" Megabytes!"); // Terminar la línea
+  //El resultado es: Su HD es 100 GB grande. Puede almacenar -28672 Megabytes!
 
 }
 
 //********************************* LOOP ******************************************
-void loop()  //no program on loop part
+void loop()  //Sin programa en la parte del bucle
 {
 }
 //******************************* FUNCIONS ****************************************
---
+++ serial_2_5_si/serial_2_5_si.ino
@@ -0,0 +1,34 @@
+/****************************************************************
+**                                                             **                                                             
+**                       titulo :                              **                                                             
+**                       serial_2_5                            **
+**                                                             ** 
+**    NOM : ABDELLAZIZ AARAB          DATA : 05/03/2017        **                       
+****************************************************************/                       
+//************************ INCUDE *******************************
+
+//*********************** VARIABLES *****************************
+//define the variable
+long drive_gb = 100; //define 100 GB
+long drive_mb; // change int for long to get more numbers
+//******************************** SETUP ******************************************
+void setup()             
+{
+Serial.begin(9600);      // run one when the arduino starts
+  Serial.print("Your HD is "); // first text on first line in terminal 
+  Serial.print(drive_gb); // punt the number defined before
+  Serial.println(" GB large."); // to end the line
+
+  drive_mb = 1024 * drive_gb; //define the conversor type between GB and Megabytes
+
+  Serial.print("It can store "); //first text on second line in terminal
+  Serial.print(drive_mb); // the number of multiply 1024*100
+  Serial.println(" Megabytes!"); // to end the line
+}
+
+//********************************* LOOP ******************************************
+void loop()  //no program on loop part
+{
+}
+//******************************* FUNCIONS ****************************************
+
